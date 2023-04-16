ad9758b43e411472b5194d979b551b8371c3adf1
SunDocker <sundocker@qq.com>
2023-04-16 17:23:32 +0800

    lab2arp

--- src/arp.c
+++ src/arp.c
@@ -5,7 +5,7 @@
 #include "ethernet.h"
 /**
  * @brief 初始的arp包
- * 
+ *
  */
 static const arp_pkt_t arp_init_pkt = {
     .hw_type16 = constswap16(ARP_HW_ETHER),
@@ -18,19 +18,19 @@ static const arp_pkt_t arp_init_pkt = {
 
 /**
  * @brief arp地址转换表，<ip,mac>的容器
- * 
+ *
  */
 map_t arp_table;
 
 /**
  * @brief arp buffer，<ip,buf_t>的容器
- * 
+ *
  */
 map_t arp_buf;
 
 /**
  * @brief 打印一条arp表项
- * 
+ *
  * @param ip 表项的ip地址
  * @param mac 表项的mac地址
  * @param timestamp 表项的更新时间
@@ -42,7 +42,7 @@ void arp_entry_print(void *ip, void *mac, time_t *timestamp)
 
 /**
  * @brief 打印整个arp表
- * 
+ *
  */
 void arp_print()
 {
@@ -53,17 +53,26 @@ void arp_print()
 
 /**
  * @brief 发送一个arp请求
- * 
+ *
  * @param target_ip 想要知道的目标的ip地址
  */
 void arp_req(uint8_t *target_ip)
 {
     // TO-DO
+    buf_init(&txbuf, sizeof(arp_pkt_t));
+    arp_pkt_t *arp_pkt = (arp_pkt_t *)txbuf.data;
+
+    memcpy(arp_pkt, &arp_init_pkt, sizeof(arp_pkt_t));
+    arp_pkt->opcode16 = constswap16(ARP_REQUEST);
+    memcpy(arp_pkt->target_ip, target_ip, NET_IP_LEN);
+
+    uint8_t broadcast_mac[NET_MAC_LEN] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+    ethernet_out(&txbuf, broadcast_mac, NET_PROTOCOL_ARP);
 }
 
 /**
  * @brief 发送一个arp响应
- * 
+ *
  * @param target_ip 目标ip地址
  * @param target_mac 目标mac地址
  */
@@ -74,7 +83,7 @@ void arp_resp(uint8_t *target_ip, uint8_t *target_mac)
 
 /**
  * @brief 处理一个收到的数据包
- * 
+ *
  * @param buf 要处理的数据包
  * @param src_mac 源mac地址
  */
@@ -85,7 +94,7 @@ void arp_in(buf_t *buf, uint8_t *src_mac)
 
 /**
  * @brief 处理一个要发送的数据包
- * 
+ *
  * @param buf 要处理的数据包
  * @param ip 目标ip地址
  * @param protocol 上层协议
@@ -93,11 +102,12 @@ void arp_in(buf_t *buf, uint8_t *src_mac)
 void arp_out(buf_t *buf, uint8_t *ip)
 {
     // TO-DO
+     
 }
 
 /**
  * @brief 初始化arp协议
- * 
+ *
  */
 void arp_init()
 {
